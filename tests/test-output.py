8368d0a4ad31beacdd2a7bf0a9453fe252a2e084
Shlomi Fish <shlomif@shlomifish.org>
2018-05-19 21:20:35 +0300

    Refactor the test suite.

--- tests/test-output.py
+++ tests/test-output.py
@@ -5,12 +5,6 @@ import unittest
 
 
 class MyTests(unittest.TestCase):
-    def _read_good_output(self):
-        txt = None
-        with open('./tests/data/24.hs.board', 'r') as f:
-            txt = f.read()
-        self._want_output = txt
-
     def _my_test_output(self, cmd, want_output):
         got_output = os.popen(cmd).read()
         self.assertEqual(
@@ -18,10 +12,9 @@ class MyTests(unittest.TestCase):
             'The command "%s" yields good output' % (cmd))
 
     def test_output(self):
-        self._read_good_output()
         self._my_test_output(
             '< ./tests/data/24.fcs.board perl ./contrib/input-from-fc-solve',
-            self._want_output)
+            open('./tests/data/24.hs.board', 'r').read())
         self._my_test_output(
             './solver.exe ./tests/data/24.hs.board',
             open('./tests/data/24.hs.output.txt', 'r').read())
